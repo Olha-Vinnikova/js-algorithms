e439ca0d485f47abcedfbea4111a45be676e9ab3
Olha <olha_vinnikova@epam.com>
2024-12-01 12:15:20 +0000

    chunk array

---
+++ chunkArray.js
@@ -0,0 +1,15 @@
+// chunkArray([1,2,3,4,5,6,7], 2)  Output: [[1,2], [3,4], [5,6]]
+
+const chunkArray = (arr, num) => {
+  const newArr = [];
+  let index = 0;
+  while (index < arr.length) {
+    newArr.push(arr.slice(index, index + num))
+    index += num;
+  }
+
+
+  return newArr;
+}
+
+console.log(chunkArray([1, 2, 3, 4, 5, 6, 7], 2))
